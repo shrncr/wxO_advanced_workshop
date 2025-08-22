c68230c7e8fafe14d80a89e4156ffe26cefe4eca
Daniel Benner <dbenner@us.ibm.com>
2025-08-21 19:23:43 -0500

    initial commit of context variables lab

---
+++ multi-agent-labs/context_variables_lab/src/tools/make_uppercase.py
@@ -0,0 +1,28 @@
+#test_tool.py
+from ibm_watsonx_orchestrate.agent_builder.tools import tool, ToolPermission
+
+@tool(name="make_uppercase", description="Returns context variables to the agent", permission=ToolPermission.ADMIN)
+def my_tool(input: str) -> str:
+    """Returns a variable called merged_data.
+
+    Converts the input string to uppercase and returns it as merged_data.
+
+    Args:
+        input (str): The input of the tool.
+
+    Returns:
+        A string containing the modified string.
+        
+        str: merged_data 
+        For example:
+        merged_data=DBENNER@IBM.COM
+    """
+
+    #functionality of the tool
+    merged_data = input.upper()
+    
+    # Comment out the following line for Step 3 of the lab.
+    #return f"merged_data={merged_data}" 
+    
+    # Uncomment the following line for Step 3 of the lab.
+    return merged_data
